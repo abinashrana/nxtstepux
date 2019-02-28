b3530b81f9d0982aec49b257e8ea29c93dcf24d8
Abinash Rana <abinash.avdesigns@gmail.com>
2019-02-28 20:41:43 +0545

    commit from home 12

--- gulpfile.js
+++ gulpfile.js
@@ -65,11 +65,11 @@ function html() {
     .pipe($.if(/\.js$/, $.uglify({compress: {drop_console: true}})))
     .pipe($.if(/\.css$/, $.postcss([cssnano({safe: true, autoprefixer: false})])))
     .pipe($.if(/\.html$/, $.htmlmin({
-      collapseWhitespace: true,
+      collapseWhitespace: false,
       minifyCSS: true,
       minifyJS: {compress: {drop_console: true}},
       processConditionalComments: true,
-      removeComments: true,
+      removeComments: false,
       removeEmptyAttributes: true,
       removeScriptTypeAttributes: true,
       removeStyleLinkTypeAttributes: true
