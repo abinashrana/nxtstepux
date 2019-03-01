7bdaefb4cddcf224e372b790dfedd1ea12f1ca75
abinash <a.rana@andmine.com>
2019-03-01 18:17:33 +0545

    13 office commit

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -349,3 +349,88 @@ var Scrollbar = (function() {
 	}
 
 })();
+
+// 
+// Calender
+// 
+
+
+
+$(document).ready(function() {
+
+  $('#calendar').fullCalendar({
+    header: {
+      center: 'title',
+      left: 'prev',
+      right: 'next'
+    },
+    buttonIcons:{
+      prev: 'angle-left',
+      next: 'angle-right'
+    },
+    dayNamesShort: ['S', 'M', 'T', 'W', 'T', 'F', 'S'],
+    theme:false,
+    defaultDate: '2019-01-12',
+    navLinks: true, // can click day/week names to navigate views
+    editable: true,
+    eventLimit: true, // allow "more" link when too many events
+    events: [
+      {
+        title: 'Asia New Zealand',
+        start: '2019-01-01'
+      },
+      {
+        title: 'Long Event',
+        start: '2019-01-07',
+        end: '2019-01-10'
+      },
+      {
+        id: 999,
+        title: 'Repeating Event',
+        start: '2019-01-09T16:00:00'
+      },
+      {
+        id: 999,
+        title: 'Repeating Event',
+        start: '2019-01-16T16:00:00'
+      },
+      {
+        title: 'Conference',
+        start: '2019-01-11',
+        end: '2019-01-13'
+      },
+      {
+        title: 'Meeting',
+        start: '2019-01-12T10:30:00',
+        end: '2019-01-12T12:30:00'
+      },
+      {
+        title: 'Lunch',
+        start: '2019-01-12T12:00:00'
+      },
+      {
+        title: 'Meeting',
+        start: '2019-01-12T14:30:00'
+      },
+      {
+        title: 'Happy Hour',
+        start: '2019-01-12T17:30:00'
+      },
+      {
+        title: 'Dinner',
+        start: '2019-01-12T20:00:00'
+      },
+      {
+        title: 'Birthday Party',
+        start: '2019-01-13T07:00:00'
+      },
+      {
+        title: 'Click for Google',
+        url: 'http://google.com/',
+        start: '2019-01-28'
+      }
+    ]
+  });
+
+});
+
